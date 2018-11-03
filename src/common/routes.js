edb79e46ef2b62055373c135861942df89f0f390
zrg-team <38390571+zrg-team@users.noreply.github.com>
2018-11-03 15:57:19 +0700

    [Upgrade] package.json (#2)
    ## Features:
    * Add gh-pages
    * Update version

--- src/common/routes.js
+++ src/common/routes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Route } from 'react-router'
 import Dashboard from '../pages/Dashboard'
 import NewProject from '../pages/NewProject'
-import Playground from '../pages/Playground'
+import ProjectPlayground from '../pages/ProjectPlayground'
 import ImportProject from '../pages/ImportProject'
 import Tools from '../pages/Tools'
 import Page from './hocs/Page'
@@ -17,7 +17,7 @@ export default class Root extends Component {
         <Route path='/dashboard' component={Page(Dashboard)} />
         <Route path='/new' component={Page(NewProject)} />
         <Route path='/import' component={Page(ImportProject)} />
-        <Route path='/playground' component={Playground} />
+        <Route path='/playground' component={ProjectPlayground} />
         <Route path='/tools' component={Tools} />
         <Route path='/setting' component={Setting} />
       </MenuPage>
